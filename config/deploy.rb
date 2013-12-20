ce2eba1c8fe535d763a3e2b142d1a071806be703
Sri Raghavan <sri.raghavan.1@gmail.com>
2013-12-20 01:46:43 -0600

    fixed deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,9 +4,9 @@ set :repository,  "https://github.com/srir/cmugrades.git"
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-ssh_options[:user] = "ubuntu"
-ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/ec2-key.pem"]
+ssh_options[:user] = "srir"
+ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/srir"]
 
 server "srir.me", :app, :web, primary: true
 set :use_sudo, false
-set :deploy_to, "/var/www/be/whatwouldmycmugrade"
+set :deploy_to, "/var/www/whatwouldmycmugradebe"
